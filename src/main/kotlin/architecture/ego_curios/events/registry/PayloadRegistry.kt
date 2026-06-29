20d7afaa2fa324fd82987173fb9f723c21b70b2a
王小尽 <1809315974@qq.com>
2026-06-29 14:06:41 +0800

    refactor(network): 简化网络载荷注册逻辑
    - 移除冗余的导入依赖
    - 使用库函数替代自定义扩展函数
    - 删除不再使用的私有方法实现
    - 优化代码结构提高可读性

--- src/main/kotlin/architecture/ego_curios/events/registry/PayloadRegistry.kt
+++ src/main/kotlin/architecture/ego_curios/events/registry/PayloadRegistry.kt
@@ -2,17 +2,10 @@ package architecture.ego_curios.events.registry
 
 import architecture.ego_curios.common.payload.toc.CurioAppurtenanceSynchroPayload
 import architecture.ego_curios.util.EgoCuriosUtil
-import architecture.goldenboughs_lib.api.payload.ToClientPayload
-import architecture.goldenboughs_lib.api.payload.ToServerAndClientPayload
-import architecture.goldenboughs_lib.api.payload.ToServerPayload
-import net.minecraft.network.RegistryFriendlyByteBuf
-import net.minecraft.network.codec.StreamCodec
-import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import architecture.goldenboughs_lib.events.registry.PayloadRegistry.playToClient
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
-import net.neoforged.neoforge.network.handling.DirectionalPayloadHandler
-import net.neoforged.neoforge.network.registration.PayloadRegistrar
 
 @EventBusSubscriber(modid = EgoCuriosUtil.ID)
 object PayloadRegistry {
@@ -22,37 +15,10 @@ object PayloadRegistry {
 		// 接收来自服务端和客户端的数据 发送到 客户端和服务端
 
 		// 接收来自服务端的数据 发送到 客户端
-		playToClient(registrar, CurioAppurtenanceSynchroPayload.TYPE, CurioAppurtenanceSynchroPayload.STREAM_CODEC)
+		registrar.playToClient(CurioAppurtenanceSynchroPayload.TYPE, CurioAppurtenanceSynchroPayload.STREAM_CODEC)
 
 		// 接收来自客户端的数据 发送到 服务端
 
 		EgoCuriosUtil.LOGGER.info("Registering payloads finish")
-	}
-
-	private fun <T : ToServerAndClientPayload> playToServerAndClient(
-		registrar: PayloadRegistrar,
-		type: CustomPacketPayload.Type<T>,
-		reader: StreamCodec<in RegistryFriendlyByteBuf, T>
-	): PayloadRegistrar {
-		return registrar.playBidirectional(
-			type, reader,
-			DirectionalPayloadHandler(ToServerAndClientPayload::handle, ToServerAndClientPayload::handle)
-		)
-	}
-
-	private fun <T : ToServerPayload> playToServer(
-		registrar: PayloadRegistrar,
-		type: CustomPacketPayload.Type<T>,
-		reader: StreamCodec<in RegistryFriendlyByteBuf, T>
-	): PayloadRegistrar {
-		return registrar.playToServer(type, reader, ToServerAndClientPayload::handle)
-	}
-
-	private fun <T : ToClientPayload> playToClient(
-		registrar: PayloadRegistrar,
-		type: CustomPacketPayload.Type<T>,
-		reader: StreamCodec<in RegistryFriendlyByteBuf, T>
-	): PayloadRegistrar {
-		return registrar.playToClient(type, reader, ToServerAndClientPayload::handle)
 	}
 }
